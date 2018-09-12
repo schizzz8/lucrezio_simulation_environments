9413a25214cac19eda208e981f6b7ef5fc7cb8d8
schizzz8 <fnardi@dis.uniroma1.it>
2018-09-12 17:55:57 +0200

    minor fix

--- src/logical_camera_plugin.cpp
+++ src/logical_camera_plugin.cpp
@@ -82,22 +82,22 @@ void LogicalCameraPlugin::OnUpdate(){
     model_msg.size.y = bounding_box.GetYLength();
     model_msg.size.z = bounding_box.GetZLength();
 
-    model_msg.min.x = bounding_box.GetCenter().x - bounding_box.GetSize().x/2.0;
-    model_msg.min.y = bounding_box.GetCenter().y - bounding_box.GetSize().y/2.0;
-    model_msg.min.z = bounding_box.GetCenter().z - bounding_box.GetSize().z/2.0;
+//    model_msg.min.x = bounding_box.GetCenter().x - bounding_box.GetSize().x/2.0;
+//    model_msg.min.y = bounding_box.GetCenter().y - bounding_box.GetSize().y/2.0;
+//    model_msg.min.z = bounding_box.GetCenter().z - bounding_box.GetSize().z/2.0;
 
-    model_msg.max.x = bounding_box.GetCenter().x + bounding_box.GetSize().x/2.0;
-    model_msg.max.y = bounding_box.GetCenter().y + bounding_box.GetSize().y/2.0;
-    model_msg.max.z = bounding_box.GetCenter().z + bounding_box.GetSize().z/2.0;
+//    model_msg.max.x = bounding_box.GetCenter().x + bounding_box.GetSize().x/2.0;
+//    model_msg.max.y = bounding_box.GetCenter().y + bounding_box.GetSize().y/2.0;
+//    model_msg.max.z = bounding_box.GetCenter().z + bounding_box.GetSize().z/2.0;
 
 
-    //        model_msg.min.x = bounding_box.min.x;
-    //        model_msg.min.y = bounding_box.min.y;
-    //        model_msg.min.z = bounding_box.min.z;
-    //
-    //        model_msg.max.x = bounding_box.max.x;
-    //        model_msg.max.y = bounding_box.max.y;
-    //        model_msg.max.z = bounding_box.max.z;
+    model_msg.min.x = bounding_box.min.x;
+    model_msg.min.y = bounding_box.min.y;
+    model_msg.min.z = bounding_box.min.z;
+
+    model_msg.max.x = bounding_box.max.x;
+    model_msg.max.y = bounding_box.max.y;
+    model_msg.max.z = bounding_box.max.z;
 
     model_msg.type = logical_image.model(i).name();
 
